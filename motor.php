8fa13e7ba9ce01b86fd90d2d55a55566a65aea4e
Daniel Czerniejewicz <programowaniedaniel@gmail.com>
2025-04-09 15:06:07 +0200

    Update motor.php

--- motor.php
+++ motor.php
@@ -7,12 +7,35 @@
     <link rel="stylesheet" href="styl.css">
 </head>
 <body>
-    
+    <main>
+        <header id = "baner"> 
+            <h1 id = "napisjeden">Motocykle - moja pasja</h1>
+        </header>
+        <aside id = "lewy"> 
+            <h2 id = "tekst_lewy"> Gdzie pojechać?</h2>
+            <?php include 'skrypt1.php'; ?>
+        </aside>
+        <section id = "prawy_jeden"> 
+            <h2 id = "tekst_prawy_jeden">Co kupić?</h2>
+            <ol>
+                <li>Honda CBR125R</li>
+                <li>Yamaha YBR125</li>
+                <li>Honda VFR800i</li>
+                <li>Honda CBR1100XX</li>
+                <li>BMW R1200GS LC</li>
+            </ol>
+        </section>
+        <aside id = "prawy_dwa"> 
+            <h2 id = "teks_prawy_dwa">Statystyki</h2>
+            <br>
+            <p> <?php include 'skrypt2.php' ?></p>
+            <br>
+            <p>Użytkowników forum: 200</p>
+            <br>
+            <p>Przesłanych zdjęć: 1300</p>
+        </aside>
+    </main>
+        <footer id = "stopka"><h1 id="napisdwa"> Strone Tworzył: Daniel Czerniejewicz</h1></footer>
     <img src="motor.png" alt="motocykl" id="motor">
-    <div id = "baner"> <h1 id = "napisjeden">Motocykle - moja pasja</h1></div>
-    <div id = "lewy"> <h2 id = "tekst_lewy"> Gdzie pojechać?</h2><?php include 'skrypt1.php'; ?></div>
-    <div id = "prawy_jeden"> <h2 id = "tekst_prawy_jeden">Co kupić?</h2><ol><li>Honda CBR125R</li><li>Yamaha YBR125</li><li>Honda VFR800i</li><li>Honda CBR1100XX</li><li>BMW R1200GS LC</li></ol></div>
-    <div id = "prawy_dwa"> <h2 id = "teks_prawy_dwa">Statystyki</h2><br><p> <?php include 'skrypt2.php' ?></p> <br><p>Użytkowników forum: 200</p><br><p>Przesłanych zdjęć: 1300</p></div>
-    <div id = "stopka"><h1 id="napisdwa"> Strone Tworzył: Jakub Jabłoński</h1></div>
 </body>
 </html>
